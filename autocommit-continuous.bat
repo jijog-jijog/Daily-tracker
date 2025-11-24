3c86db31673c5cbc4f7344b668b10657a4971944
Jijo G <145054528+jijog-jijog@users.noreply.github.com>
2025-11-24 22:46:34 +0530

    Update autocommit-continuous.bat

--- autocommit-continuous.bat
+++ autocommit-continuous.bat
@@ -1,5 +1,4 @@
 @echo off
 cd /d "d:\project\Streek\D
 ily-tracker"
-powershell.exe -ExecutionPolicy Bypass -File "autocommit-continuous.p
-
+powershell.exe -ExecutionPolicy Bypass -File "autocommit-continuo
